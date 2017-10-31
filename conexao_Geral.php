2cf9a0496a75c9d227ab1936df5e4918bcc0edc8
rafanonato <rf.nonato@gmail.com>
2017-10-30 22:25:25 -0200

    Alteração de endereços

--- conexao_Geral.php
+++ conexao_Geral.php
@@ -1,8 +1,8 @@
 <?php 
 
-/*$_host    =    "mysql02.cpscetec.com.br";
-$_usuario  =    "mapeamento";
-$_senha    =    "r2d2@42";
+/*$_host    =    "#";
+$_usuario  =    "#";
+$_senha    =    "#";
 */
 if(!isset($_SESSION)){
   session_start();
@@ -13,27 +13,27 @@ $periodo2=$_SESSION['periodo'];
 
 if($periodo2=='2012'){
 
-$_host = "mysql02.cpscetec.com.br";
-$_usuario = "mapeamento";
-$_senha = "r2d2@42";
+$_host = "#";
+$_usuario = "#";
+$_senha = "#@#";
 $_db      = mysql_connect($_host,$_usuario,$_senha) or die ("Não foi possivel Conectar so Servidor");
 mysql_select_db("mapeamento",$_db) or die ("Não Foi Possivel Conectar ao Banco de Dados");
 
 
 }elseif($periodo2=='2013'){
 
-$_host = "mysql-l50cnn0667.locaweb.com.br";
-$_usuario = "mapeamento2013";
-$_senha = "r2d2@42";
+$_host = "#";
+$_usuario = "#";
+$_senha = "#";
 $_db      = mysql_connect($_host,$_usuario,$_senha) or die ("Não foi possivel Conectar so Servidor");
 mysql_select_db("mapeamento2013",$_db) or die ("Não Foi Possivel Conectar ao Banco de Dados");
 
 
 }elseif($periodo2=='2015'){
 
-$_host = "mysql02.cpscetec.com.br";
-$_usuario = "mapeamento2015";
-$_senha = "r2d2@42";
+$_host = "#";
+$_usuario = "#";
+$_senha = "#";
 $_db      = mysql_connect($_host,$_usuario,$_senha) or die ("Não foi possivel Conectar so Servidor");
 mysql_select_db("mapeamento2015",$_db) or die ("Não Foi Possivel Conectar ao Banco de Dados");
 
@@ -41,9 +41,9 @@ mysql_select_db("mapeamento2015",$_db) or die ("Não Foi Possivel Conectar ao Ba
   }
   else{
 
-$_host = "mysql02.cpscetec.com.br";
-$_usuario = "mapeamento2015";
-$_senha = "r2d2@42";
+$_host = "#";
+$_usuario = "#";
+$_senha = "#";
 $_db      = mysql_connect($_host,$_usuario,$_senha) or die ("Não foi possivel Conectar so Servidor");
 mysql_select_db("mapeamento2015",$_db) or die ("Não Foi Possivel Conectar ao Banco de Dados");
 
